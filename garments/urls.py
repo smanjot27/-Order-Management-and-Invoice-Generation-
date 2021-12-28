7990cb310d4169521dad65d74c8bbc2255e982bf
Manjot Kaur <43288211+smanjot27@users.noreply.github.com>
2021-12-28 17:46:35 +0530

    Update urls.py

--- garments/urls.py
+++ garments/urls.py
@@ -1,7 +1,8 @@
 from garments import views
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path,include, re_path
+import re
+#handler404 = 'garments.views.view_404'
 urlpatterns = [
     path('', views.FullDaySales),
     path('fullday',views.FullDaySales),
@@ -14,7 +15,9 @@ urlpatterns = [
     path('tailorSlip', views.tailorSlip),
     path('receivedslip', views.OrderReceivedSlip),
     path('completedslip', views.OrderCompletedSlip),
-    path('completedorders', views.Completedorders)
-
+    path('completedorders', views.Completedorders),
+    path('edittailors/<int:tid>', views.EditTailor),
+    path('editorder/<int:oid>', views.EditOrder),
+    re_path('^edit.*/$', views.view_404)
 ]
 
